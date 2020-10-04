fa0b37dcad1e3ffd45c55a20669941f95bac306a
Phillip <phillip.mittmann@gmail.com>
2020-10-04 18:29:24 -0300

    Aula console.log()

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+function soma(a, b) {
+    return a + b;
+}
+
+console.log(soma(10, 20));
+
+
+/*
+var mostrarSite = false;
+const site = "www.guiadoprogramador.com";
+
 console.log("Hello world");
+
+if(mostrarSite) {
+    console.log(site);
+}
+*/
